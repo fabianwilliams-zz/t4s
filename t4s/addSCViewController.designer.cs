ea0dd40e11096108ea0eafbea85d6fa044a384fc
Fabian Williams <fabian@adotob.com>
2015-02-21 15:02:03 -0500

    Complted StoryBoard draft Alpha for all View Controllers

--- t4s/addSCViewController.designer.cs
+++ t4s/addSCViewController.designer.cs
@@ -14,8 +14,64 @@ namespace t4s
 	[Register ("addSCViewController")]
 	partial class addSCViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnAddEvent { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel lblRecordAdded { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtEventHasttag { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtSessionName { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtSessionNumber { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtSpeakerName { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtSpeakerTwitter { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnAddEvent != null) {
+				btnAddEvent.Dispose ();
+				btnAddEvent = null;
+			}
+			if (lblRecordAdded != null) {
+				lblRecordAdded.Dispose ();
+				lblRecordAdded = null;
+			}
+			if (txtEventHasttag != null) {
+				txtEventHasttag.Dispose ();
+				txtEventHasttag = null;
+			}
+			if (txtSessionName != null) {
+				txtSessionName.Dispose ();
+				txtSessionName = null;
+			}
+			if (txtSessionNumber != null) {
+				txtSessionNumber.Dispose ();
+				txtSessionNumber = null;
+			}
+			if (txtSpeakerName != null) {
+				txtSpeakerName.Dispose ();
+				txtSpeakerName = null;
+			}
+			if (txtSpeakerTwitter != null) {
+				txtSpeakerTwitter.Dispose ();
+				txtSpeakerTwitter = null;
+			}
 		}
 	}
 }
--- t4s/tweetnowViewController.designer.cs
+++ t4s/tweetnowViewController.designer.cs
@@ -14,8 +14,32 @@ namespace t4s
 	[Register ("tweetnowViewController")]
 	partial class tweetnowViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnT4S { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel lblTweetRecordAdded { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextView txtMyTweet { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnT4S != null) {
+				btnT4S.Dispose ();
+				btnT4S = null;
+			}
+			if (lblTweetRecordAdded != null) {
+				lblTweetRecordAdded.Dispose ();
+				lblTweetRecordAdded = null;
+			}
+			if (txtMyTweet != null) {
+				txtMyTweet.Dispose ();
+				txtMyTweet = null;
+			}
 		}
 	}
 }
