560d3d6a781ea00cf87c65cd5b51c1387b6e4b3e
Fabian Williams <fabian@adotob.com>
2015-03-21 23:12:19 -0400

    Adding Policy and Procedure Rules UIWebView

---
+++ t4s/PolicyandRules.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace t4s
+{
+	partial class PolicyandRules : UIViewController
+	{
+		public PolicyandRules (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
