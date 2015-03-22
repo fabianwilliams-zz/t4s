d58c832ec32764ed348803edf5c7c9d363d3618d
Fabian Williams <fabian@adotob.com>
2015-03-21 23:16:52 -0400

    Adding Policy and Procedure Rules UIWebView with backing class

--- t4s/PolicyandRules.cs
+++ t4s/PolicyandRules.cs
@@ -7,8 +7,21 @@ namespace t4s
 {
 	partial class PolicyandRules : UIViewController
 	{
-		public PolicyandRules (IntPtr handle) : base (handle)
+		public override void ViewDidLoad ()
 		{
+			base.ViewDidLoad ();
+
+			Title = "Tweet4Swag Official Rules";
+			View.BackgroundColor = UIColor.White;
+
+			webView = new UIWebView(View.Bounds);			
+			View.AddSubview(webView);
+
+			string url = "http://www.fabiangwilliams.com/official-rules-and-privacy/";
+			webView.LoadRequest (new NSUrlRequest (new NSUrl (url)));
+
+			// if this is false, page will be 'zoomed in' to normal size
+			webView.ScalesPageToFit = true;
 		}
 	}
 }
